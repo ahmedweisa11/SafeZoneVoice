25969faa53aa3fc9e634b032a9efad872b15de66
ahmedweisa11 <ahmedweisa11@gmail.com>
2026-06-11 00:28:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -156,8 +156,10 @@ io.on("connection", (socket) => {
 
   });
 
-const PORT = process.env.PORT || 3000;
+  }); // <-- دي كانت ناقصة
 
-server.listen(PORT, () => {
-  console.log("Server running on", PORT);
-});
+  const PORT = process.env.PORT || 3000;
+
+  server.listen(PORT, () => {
+    console.log("Server running on", PORT);
+  });
